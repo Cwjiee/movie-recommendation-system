5f8e3e5e7c608d885a3fd653137d9d166b9a750a
Wei Jie <jackchong398@gmail.com>
2023-11-26 14:46:50 +0800

    feat: add year wise analysis

---
+++ pages/Year_Wise_Analysis.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import data
+
+st.set_page_config(page_title='Year Wise Analysis')
+
+st.title('Year Wise Analysis')
+
+df = data.load_data()
+df.dropna(inplace=True)
+
+fig = px.histogram(df, y="release_year", color="release_year")
+
+fig.show()
+
+st.plotly_chart(fig)
